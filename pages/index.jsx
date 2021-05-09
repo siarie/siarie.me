5525200e2dd2aa85a4dba3861f111064e932fded
Sri Aspari <sriaspari@gmail.com>
2021-05-09 11:52:34 +0700

    refactor: ♻️ preparation for migration to nextjs
    Signed-off-by: Sri Aspari <sriaspari@gmail.com>

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import home from '../styles/home.module.css';
+
+function Index() {
+	return (
+		<div className={home.wrapper}>
+			<h1>Hello World</h1>
+		</div>
+	);
+}
+
+export default Index;
--- src/filters.js
+++
@@ -1,8 +0,0 @@
-import Vue from "vue";
-import moment from "moment";
-
-Vue.filter("fromNow", function(value) {
-  if (value) {
-    return moment(value, "YYYYMMDD").fromNow();
-  }
-});
